93c9327d35d12a33cc30fb1f92688cc6d0f23a44
Tyler Longren <tlongren@gmail.com>
2011-02-14 15:40:14 -0600

    Added files for thumbnail-viewer 1.2!

---
+++ README.txt
@@ -0,0 +1,49 @@
+=== Thumbnail Viewer ===
+Tags: images, javascript, lightbox, image, thumbnail, quicktag
+Contributors: tlongren
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=tlongren%40gmail%2ecom&item_name=Donation%20For%20WordPress%20Theme%20or%20Plugin&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8
+Requires at least: 2.1
+Tested up to: 2.3-alpha
+Stable tag: 1.2
+
+A minimalist image viewer with an effect similar to Lightbox JS, only much faster with a whole lot less Javascript.
+
+== Description ==
+
+[Thumbnail Viewer](http://www.longren.org/wordpress/thumbnail-viewer/ "Thumbnail Viewer")  is simply used to overlay images on a page, similar to Lightbox JS.  The javascript is from the Image Thumbnail Viewer code from [DynamicDrive](http://www.dynamicdrive.com/dynamicindex4/thumbnail.htm "DynamicDrive").  The javascript included in Thumbnail Viewer from DynamicDrive is much faster than Lightbox JS, although not as feature packed, which is the entire point.
+
+Thumbnail Viewer works with the following image formats:
+png
+gif
+jpeg
+jpg
+bmp
+
+== Installation ==
+
+1. Extract thumbnail-viewer folder from downloaded file to wp-contents/plugins/.
+2. Go to Plugins section in WordPress dashboard and activate Thumbnail Viewer.
+3. That’s it!
+4. Now all you need to do is add rel=thumbnail to any link tag surrounding your thumbnail.  You'll do this when you're editing a post or page, you can also use the quicktag button for auto-insertion goodness.
+
+== Frequently Asked Questions ==
+
+= Is Thumbnail Viewer a photo manager? =
+
+Absolutely not.  It simply displays large versions of smaller images (thumbnails) in an unobtrusive yet stylish overlay.
+
+= Can images in the form of WordPress attachments be displayed? =
+
+Nope, see previous answer.
+
+= Where can I find support? =
+Visit [http://www.longren.org/](http://www.longren.org/contact/ "longren.org") and get in touch with me (Tyler) there.
+
+= Why can't I see the quicktag when I'm writing a new post or editing an existing post? =
+
+If you're using the Visual Editor, click on the Code tab, you'll see the quicktag there.
+
+== Screenshots ==
+
+1. Thumbnail Viewer with some corn.
+2. Thumbnail Viewer showing the pretty new Xbox 360 Elite.
---
+++ wp-thumbnailviewer.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Plugin Name: Thumbnail Viewer
+Plugin URI: http://www.longren.org/wordpress/thumbnail-viewer/
+Description: <a href="http://www.longren.org/wordpress/thumbnail-viewer/">Thumbnail Viewer</a> is a simple plugin for showing larger images of your thumbnails. It's very similar to Lightbox, only much smaller. Add the rel="thumbnail" attribute to any link tag to activate the thumbnail viewer. It's based the <a href="http://zeo.unic.net.my/notes/lightbox-js-version-20/">wp-lightbox plugin</a> and on the <a href="http://www.dynamicdrive.com/dynamicindex4/thumbnail.htm">Image Thumbnail Viewer from Dynamic Drive</a>.
+Version: 1.2
+Author: Tyler Longren
+Author URI: http://www.longren.org/
+License: Creative Commons Attribution-ShareAlike
+*/
+
+define("IMAGE_FILETYPE", "(bmp|gif|jpeg|jpg|png)", true);
+
+function wp_ddimageviewer_init() {
+	$url = get_bloginfo('wpurl');
+?>
+	<!-- WP Dynamic Drive Thumbnail Image Viewer Plugin -->
+	<?php
+	echo '<script type="text/javascript">'."\n";
+	echo '/* <![CDATA[ */'."\n";
+	echo "\t".'var loading_url = \''.get_option('siteurl').'/wp-content/plugins/thumbnail-viewer/images/loadingImage.gif\';'."\n";
+	echo '/* ]]> */'."\n";
+	echo '</script>'."\n";
+	?>
+	<link rel="stylesheet" href="<?php echo $url; ?>/wp-content/plugins/thumbnail-viewer/css/thumbnailviewer.css" type="text/css" media="screen" />
+	<script src="<?php echo $url; ?>/wp-content/plugins/thumbnail-viewer/js/thumbnailviewer.js" type="text/javascript">
+	</script>
+<?php }
+
+function wp_ddimageviewer_add_quicktag() {
+	if (strpos($_SERVER['REQUEST_URI'], 'post-new.php') || strpos($_SERVER['REQUEST_URI'], 'page-new.php')) {
+?>
+<script type="text/javascript">//<![CDATA[
+	var toolbar = document.getElementById("ed_toolbar");
+<?php
+	edit_insert_button("Thumbnail Viewer", "wp_ddimageviewer_handler", "Thumbnail Viewer with Caption");
+?>
+	var state_my_button = true;
+
+function wp_ddimageviewer_handler() {
+	if (state_my_button) {
+		var myURL = prompt('Enter the original image URL (required)', 'http://');
+		var myCaption = prompt('Enter image caption');
+		var myIMG = prompt('Enter the Image thumbnail (required)', 'http://');
+		var myWidth = prompt('Enter Width of image thumbnail (required)');
+		var myHeight = prompt('Enter Height of image thumbnail (required)');
+		var myAlt = prompt('Enter a description of the image');
+		if (myURL && myIMG && myWidth && myHeight) {
+			myValue = '<a href="'+myURL+'" rel="thumbnail" title="'+myCaption+'"><img src="'+myIMG+'" width="'+myWidth+'" height="'+myHeight+'" alt="'+myAlt+'" /></a>';
+			edInsertContent(edCanvas, myValue); 
+		}
+	}
+}
+//]]></script>
+
+<?php } }
+
+if (!function_exists('edit_insert_button')) {
+	//edit_insert_button: Inserts a button into the editor
+	function edit_insert_button($caption, $js_onclick, $title = '')	{
+	?>
+	if (toolbar) {
+		var theButton = document.createElement('input');
+		theButton.type = 'button';
+		theButton.value = '<?php echo $caption; ?>';
+		theButton.onclick = <?php echo $js_onclick; ?>;
+		theButton.className = 'ed_button';
+		theButton.title = "<?php echo $title; ?>";
+		theButton.id = "<?php echo "ed_{$caption}"; ?>";
+		toolbar.appendChild(theButton);
+	}
+	
+<?php } }
+
+function wp_ddimageviewer_replace($string) {
+	$pattern = '/(<a(.*?)href="([^"]*.)'.IMAGE_FILETYPE.'"(.*?)><img)/ie';
+  	$replacement = '(strstr("\2\5","rel=") ? "\1" : "<a\2href=\"\3\4\"\5 rel=\"thumbnail\"><img")';
+	return preg_replace($pattern, $replacement, $string);
+}
+
+add_action('wp_head', 'wp_ddimageviewer_init');
+add_filter('admin_footer', 'wp_ddimageviewer_add_quicktag');
+add_filter('the_content', 'wp_ddimageviewer_replace');
+
+?>
